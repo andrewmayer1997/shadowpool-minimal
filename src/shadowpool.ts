ca9196812439220362ed4388368ec7714d929489
Andrew Mayer <andrewmayer1997@gmail.com>
2021-03-03 19:22:26 +0000

    add err handler

--- src/shadowpool.ts
+++ src/shadowpool.ts
@@ -3,22 +3,25 @@ import { stratum } from "./core/server";
 import "./core/daemon";
 import { start } from "./core/daemon";
 import { view } from "./core/view/ttyout";
+import remote from "./core/utils/remoteLogger";
+import log from "./core/utils/logger";
 //import { back } from "./core/view/server";
 
-try {
-  stratum.create(8008, "0.0.0.0");
-  //back.createWebSocket(2002)
-  start();
-  view();
-} catch (e) {
-  console.log(`<----------------- ERROR! ----------------->`);
-  console.error(e);
-}
-
 process.on("uncaughtException", function (err) {
-  console.error("Caught exception: " + err);
+  log.error("Caught exception: " + err);
+  remote.fatal(`<----------------- ERROR! ----------------->`);
+  remote.fatal(err);
+  remote.fatal(`<----------------- ERROR! ----------------->`);
 });
 
 process.on("unhandledRejection", function (err) {
-  console.error("Caught rejection: " + err);
+  log.error("Caught rejection: " + err);
+  remote.fatal(`<----------------- ERROR! ----------------->`);
+  remote.fatal(err);
+  remote.fatal(`<----------------- ERROR! ----------------->`);
 });
+
+stratum.create(8008, "0.0.0.0");
+//back.createWebSocket(2002)
+start();
+view();
