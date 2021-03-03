2b61e89b606de107cad95e0dcbaa00aea2d7b2e3
Andrew Mayer <andrewmayer1997@gmail.com>
2021-03-03 02:42:00 +0000

    fix all

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,10 @@ module.exports = {
     filename: "shadow.js",
   },
   resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
     extensions: [".ts", ".js"],
   },
   module: {
     rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       { test: /\.tsx?$/, loader: "ts-loader" },
     ],
   },
